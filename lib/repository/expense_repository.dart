fee8b569a5fe6c22835e878535c83686d1e6a952
Sandip <sandipramanik07@gmail.com>
2022-08-24 21:15:32 +0530

    Add `expenseInLast7Days` method in expense repository

--- lib/repository/expense_repository.dart
+++ lib/repository/expense_repository.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_objectbox_example/entities/entities.dart';
+import 'package:flutter_objectbox_example/objectbox.g.dart' as objectbox_g;
 import 'package:objectbox/objectbox.dart';
 
 class ExpenseRepository {
@@ -20,5 +21,20 @@ class ExpenseRepository {
         .query()
         .watch(triggerImmediately: true)
         .map<List<Expense>>((query) => query.find());
+  }
+
+  Stream<double> expenseInLast7Days() {
+    return store
+        .box<Expense>()
+        .query(objectbox_g.Expense_.date.greaterThan(
+          DateTime.now()
+                  .subtract(const Duration(days: 7))
+                  .microsecondsSinceEpoch * 1000,
+        ))
+        .watch(triggerImmediately: true)
+        .map<double>((query) => query
+            .find()
+            .map<double>((e) => e.amount)
+            .reduce((value, element) => value + element));
   }
 }
